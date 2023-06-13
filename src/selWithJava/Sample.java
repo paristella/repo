9f972f7efd0d343c3476ef06c34fb23a3e55f856
Arpana <muriki.arpana@gmail.com>
2023-06-14 04:56:30 +0530

    Second Commit

---
+++ src/selWithJava/Sample.java
@@ -0,0 +1,10 @@
+package selWithJava;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Git And Github...");
+	}
+
+}
